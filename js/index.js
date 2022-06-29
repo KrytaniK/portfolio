93420e3f8d5c4324e283b18930d74fbab7319a17
KrytaniK <krytanik@gmail.com>
2022-06-29 13:45:08 -0500

    Added Page Interactions + Styling adjustments

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
+
+/* --------- Contact Form Setup using EmailJS ---------- */
+            // https://www.emailjs.com/docs/
+
+emailjs.init("-VHsHLzghCLGrCT4x");
+
+const contactForm = document.getElementById('form-contact');
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    contactForm.contact_number.value = Math.random() * 100000 | 0;
+    emailjs.sendForm("service_afq2pps", "template_i7gq3sx", contactForm)
+        .then(
+            () => console.log('Success')
+        ).catch(
+            (err) => console.log(err)
+        );
+})
+
+/* ---------- Page Interaction ---------- */
+
+// Scroll To Elements
+
+const ScrollTo = (className) => {
+    const element = document.querySelector(className);
+
+    if (!element) return;
+    
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+}
+
 const projectScrollTo = document.getElementById('projects-scrollTo'),
     aboutScrollTo = document.getElementById('about-scrollTo'),
     contactScrollTo = document.getElementById('contact-scrollTo');
@@ -16,11 +47,3 @@ contactScrollTo.addEventListener('click', (event) => {
     event.preventDefault();
     ScrollTo('.contact');
 });
-
-const ScrollTo = (className) => {
-    const element = document.querySelector(className);
-
-    if (!element) return;
-    
-    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-}
